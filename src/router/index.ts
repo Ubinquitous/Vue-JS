cb4ce30a3b5b8c6058bb91bd64af1f60f3889880
Ubinquitous <ubinquitous1@gmail.com>
2022-09-30 00:30:49 +0900

    ADD : webpackChunkName 설정, router 추가

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
@@ -10,10 +12,12 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+  },
+  {
+    path: '/data',
+    name: 'data',
+    component: () => import('../views/DataBindingStringView.vue'),
   }
 ]
 
