c6236a5340b460829ceb034f0c0bd9f3d76e2059
Ubinquitous <ubinquitous1@gmail.com>
2022-09-30 00:31:26 +0900

    ADD : 변수 핸들링

---
+++ src/views/DataBindingStringView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>
+            {{ message }}, {{ userName }}
+        </h1>
+        <p>your array is {{ array }}.
+            <br/>
+            and your object is {{ object }}.</p>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            userName: 'Ubin Park',
+            message: 'Hello',
+            array: [],
+            object: {},
+        }
+    }
+}
+</script>
+<style scoped>
+.text-red {
+    color: red;
+}
+</style>
