ed1b74a2b9ac23da79d3fe459787a166e5ac7ace
Ubinquitous <ubinquitous1@gmail.com>
2022-10-02 02:33:00 +0900

    ADD : htmlString 연습

--- src/views/DataBindingHtmlView.vue
+++ src/views/DataBindingHtmlView.vue
@@ -1,10 +1,18 @@
 <template>
-  
+    <div>
+        <div>{{ htmlString }}</div>
+        <div v-html="htmlString"></div>
+    </div>
 </template>
 
 <script>
 export default {
-
+    components: {},
+    data(){
+        return {
+            htmlString: '<p style="color:red;">빨간색 문자</p>'
+        }
+    }
 }
 </script>
 
