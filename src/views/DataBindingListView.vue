ff31a1494b38ed9c63ac647ea29e517f2a070e00
Ubinquitous <ubinquitous1@gmail.com>
2022-10-02 03:14:46 +0900

    ADD : map 연습

---
+++ src/views/DataBindingListView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div :key="city.code" v-for="city in cities">
+        {{ city.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            cities: [
+                {title : '서울', code: '1'},
+                {title : '부산', code: '2'},
+                {title : '대구', code: '3'},
+                {title : '김해', code: '4'}
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
